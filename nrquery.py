20cf67b0bb5ad481e72fff122e8a693fc83656dd
Vladimir Ulogov <vladimir.ulogov@me.com>
2022-05-02 18:41:15 -0600

    class Query, class Result and Result.Json()

--- nrquery.py
+++ nrquery.py
@@ -1,11 +1,68 @@
-"""This is just a sample file"""
+"""New Relic Query interface"""
+
+import os
+import json
+import requests
+from typing import Any
+
+GQLAPI = "https://api.newrelic.com/graphql"
+
+QTPL = """
+{
+   actor {
+      account(id: %s) {
+         nrql(query: "%s") {
+            results
+         }
+      }
+   }
+}
+"""
 
 
-def main() -> bool:
-    """Main"""
-    return True
+class NREnvError(Exception):
+    pass
 
 
-def squared(value: int) -> int:
-    """Returns the squared value"""
-    return value * value
+class NRResultError(Exception):
+    pass
+
+
+def get_env_data(name: str, val: Any) -> Any:
+    if val is not None:
+        return val
+    if name in os.environ.keys():
+        return os.environ.get(name)
+    raise NREnvError("Error getting value for %s" % name)
+
+
+class Query:
+    def __init__(self, account: Any = None, api_key: Any = None):
+        self.NRACCOUNT = get_env_data("NRACCOUNT", account)
+        self.NRAPIKEY = get_env_data("NRAPIKEY", api_key)
+
+    def Run(self, query: str) -> Any:
+        hdr = {"Content-Type": "application/json", "API-Key": self.NRAPIKEY}
+        qry = QTPL % (self.NRACCOUNT, query)
+        data = json.dumps({"query": qry})
+        r = requests.post(GQLAPI, data=data, headers=hdr)
+        res = Result(query, r)
+        if not res.IsSuccess:
+            raise NRResultError("Query: %s not succesfull" % query)
+        return res
+
+
+class Result:
+    def __init__(self, query: str, value: Any):
+        self.Query = query
+        self.Value = value
+        if self.Value.status_code == 200:
+            self.IsSuccess = True
+        else:
+            self.IsSuccess = False
+        self.Elapsed = self.Value.elapsed
+
+    def Json(self) -> Any:
+        if self.IsSuccess:
+            return self.Value.json()["data"]["actor"]["account"]["nrql"]["results"]
+        raise NRResultError("Query: %s returned failure" % self.Query)
